9bd7a19c20e1691fc52da832471d1c217edbaf6e
Yaman <yamanserajbs@gmail.com>
2023-05-22 14:01:09 +0300

    Added error detection

--- src/main/java/eformer/front/eformer_frontend/Main.java
+++ src/main/java/eformer/front/eformer_frontend/Main.java
@@ -1,6 +1,10 @@
 package eformer.front.eformer_frontend;
 
+import eformer.front.eformer_frontend.connector.ItemsConnector;
 import eformer.front.eformer_frontend.connector.RequestsGateway;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -8,6 +12,7 @@ import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.File;
 import java.io.IOException;
@@ -31,22 +36,44 @@ public class Main extends Application {
         /* Patches the illegalAccess issues of jfoenix */
         org.burningwave.core.assembler.StaticComponentContainer.Modules.exportAllToAll();
 
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/views/Dashboard.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-
-        scene.addEventFilter(KeyEvent.KEY_PRESSED, ke -> {
-            if (ke.getCode() == KeyCode.ESCAPE) {
-                System.exit(0);
-            }
-        });
-
-        stage.setTitle("eFormer");
-        stage.setResizable(true);
-        stage.setMaximized(true);
         stage.getIcons().clear();
         stage.getIcons().add(getImage("cart.png"));
         stage.setFullScreenExitHint("");
-        stage.setScene(scene);
+        stage.setTitle("eFormer");
+
+        Splash splash = new Splash();
+        splash.show();
+        stage.setScene(splash.getSplashScene());
+
+        splash.getSequentialTransition().setOnFinished(e -> {
+            Timeline timeline = new Timeline();
+            KeyFrame key = new KeyFrame(Duration.millis(800),
+                    new KeyValue(splash.getSplashScene().getRoot().opacityProperty(), 0));
+            timeline.getKeyFrames().add(key);
+            timeline.setOnFinished((event) -> {
+                try {
+                    FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/views/Dashboard.fxml"));
+                    Scene scene = new Scene(fxmlLoader.load());
+
+                    scene.addEventFilter(KeyEvent.KEY_PRESSED, ke -> {
+                        if (ke.getCode() == KeyCode.ESCAPE) {
+                            System.exit(0);
+                        }
+                    });
+
+                    stage.setScene(scene);
+                    stage.setResizable(true);
+                    stage.setMaximized(true);
+                }
+                catch (IOException ex) {
+                    RequestsGateway.displayException(ex);
+                }
+            });
+            timeline.play();
+        });
+
+        stage.setWidth(450);
+        stage.setHeight(280);
         stage.show();
     }
 
